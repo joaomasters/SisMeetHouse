4ea61a13693df0d56f72ed24ee0a2f2dddd8e19e
João Gabriel Pereira <30707586+joaomasters@users.noreply.github.com>
2026-09-05 19:46:54 -0300

    chore: adiciona application-local.yml, docker-compose.dev.yml e remove migration V7 duplicado

--- backend/src/main/resources/db/migration/V7_add_bebida_tipo_produto.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE produtos DROP CONSRAINT IF EXISTS produtos_tipo_produto_check;
-
-ALTER TABLE produtos ADD CONTRAINT produtos_tipo_produto_check CHECK (tipo_produto IN ('CORTE', 'INDUSTRIALIZADO', 'INSUMO', 'SUBPRODUTO', 'BEBIDA', 'EMBALAGEM', 'SERVIÇO', 'OUTROS')); 
